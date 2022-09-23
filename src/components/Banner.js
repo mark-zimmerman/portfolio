4684d2d083f2c0cedd01889e27e46df5c3d7ff04
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-09-23 14:22:38 -0400

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,16 +9,28 @@ const Banner = () => {
             <h2 className="medium-main-text">My Work</h2>
             <div id="project-card-container">  
                 <div className="project-card strangers-card" onClick={() => openInNewTab('https://6324de9061e5f400081defe4--cheery-sopapillas-b87876.netlify.app/')}>
-                    <h3 >Stranger's Things</h3>
+                    <div className="inside-card">   
+                        <h3>Stranger's Things</h3>
+                        <button class="display-none">Open Project</button>
+                    </div> 
                 </div>
                 <div className="project-card truthy-card" onClick={()=> openInNewTab('https://truthy-jerseyz-outlet.herokuapp.com/')}>
-                    <h3>Truthy Jerseyz Outlet</h3>
+                    <div className="inside-card">     
+                        <h3>Truthy Jerseyz Outlet</h3>
+                        <button class="display-none">Open Project</button>
+                    </div>
                 </div>
                 <div className="project-card connect-four-card" onClick={() => openInNewTab('https://charming-scone-2535b3.netlify.app/')}>
-                    <h3>Connect Four</h3>
+                    <div className="inside-card">     
+                        <h3>Connect Four</h3>
+                        <button class="display-none">Open Project</button>
+                    </div>
                 </div>
                 <div className="project-card fitness-tracker-card">
-                    <h3>Fitness Tracker</h3>
+                    <div className="inside-card"> 
+                        <h3>Fitness Tracker</h3>
+                        <button class="display-none">Open Project</button>
+                    </div>
                 </div>
             </div>
         </div>
