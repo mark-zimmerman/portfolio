4e8ee6d486427690347aace846e41692fca89532
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-09-25 19:05:00 -0400

    projects hover effect

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,25 +11,29 @@ const Banner = () => {
                 <div className="project-card strangers-card" onClick={() => openInNewTab('https://6324de9061e5f400081defe4--cheery-sopapillas-b87876.netlify.app/')}>
                     <div className="inside-card">   
                         <h3>Stranger's Things</h3>
-                        <button class="display-none">Open Project</button>
+                        <p className="project-p">This paragraph will quickly explain the project</p>
+                        <button class="project-btn">Open Project</button>
                     </div> 
                 </div>
                 <div className="project-card truthy-card" onClick={()=> openInNewTab('https://truthy-jerseyz-outlet.herokuapp.com/')}>
                     <div className="inside-card">     
                         <h3>Truthy Jerseyz Outlet</h3>
-                        <button class="display-none">Open Project</button>
+                        <p className="project-p">This paragraph will quickly explain the project</p>
+                        <button class="project-btn">Open Project</button>
                     </div>
                 </div>
                 <div className="project-card connect-four-card" onClick={() => openInNewTab('https://charming-scone-2535b3.netlify.app/')}>
                     <div className="inside-card">     
                         <h3>Connect Four</h3>
-                        <button class="display-none">Open Project</button>
+                        <p className="project-p">This paragraph will quickly explain the project</p>
+                        <button class="project-btn">Open Project</button>
                     </div>
                 </div>
                 <div className="project-card fitness-tracker-card">
                     <div className="inside-card"> 
                         <h3>Fitness Tracker</h3>
-                        <button class="display-none">Open Project</button>
+                        <p className="project-p">This paragraph will quickly explain the project</p>
+                        <button class="project-btn">Open Project</button>
                     </div>
                 </div>
             </div>
